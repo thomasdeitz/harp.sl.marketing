498c6d5d5591b4f983ad00b417a21af2d58f5021
Tom Deitz <info@thomasdeitz.com>
2018-01-24 08:14:06 -0700

    deploy script update

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ echo "Getting deployment target."
 
 TARGET=$STAGING_TARGET
 
-echo "Getting source information for $TRAVIS_REPO_SLUG"
+echo "Getting source harp.js code from $TRAVIS_REPO_SLUG"
 CURRENT_REPO_SLUG=$(echo $TRAVIS_REPO_SLUG | cut -d "/" -f2 )
 CURRENT_COMMIT=`git rev-parse HEAD`
 
@@ -13,16 +13,16 @@ echo "Targeting $TARGET repository"
 TARGET_URL='https://github.com/thomasdeitz/'$TARGET'.git'
 TARGET_URL_WITH_CREDENTIALS=${TARGET_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github.com}
 
-echo "Cloning repository from ${TARGET_URL}"
+echo "Cloning repository from $TARGET_URL"
 cd ../
 git clone $TARGET_URL || exit 1
 cd $TARGET || exit 1
 
-echo "Checking out master from ${TARGET}"
+echo "Checking out master from $TARGET"
 git checkout master || exit 1
 cd ../
 
-echo "Compiling new static content for ${TARGET_URL}"
+echo "Compiling new static content for $TARGET_URL"
 harp compile $CURRENT_REPO_SLUG $TARGET || exit 1
 cd $TARGET
 
@@ -39,8 +39,8 @@ git push --force --quiet "$TARGET_URL_WITH_CREDENTIALS" || exit 1
 cd ../
 
 echo "Cleaning up temp files"  
-rm -Rf $TARGET
-rm -Rf $CURRENT_REPO_SLUG
+rm -Rf $TARGET || exit 1
+rm -Rf $CURRENT_REPO_SLUG || exit 1
 
 echo "Deployed successfully."  
-exit 0 
+exit 0
