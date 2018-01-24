ec196e280bdf71736e2cefe817c19f1d1c7151ee
Tom Deitz <info@thomasdeitz.com>
2018-01-24 08:00:42 -0700

    Updates deploy script to use more variables for reuse.

--- deploy.sh
+++ deploy.sh
@@ -4,35 +4,43 @@ echo "Starting deployment"
 echo "Getting deployment target."
 
 TARGET=$STAGING_TARGET
-echo $TRAVIS_REPO_SLUG
-echo "Targeting $TARGET repository"
 
-CURRENT_REPO_SLUG=$TRAVIS_REPO_SLUG | cut -d "/" -f 2
+echo "Getting source information for $TRAVIS_REPO_SLUG"
+CURRENT_REPO_SLUG=$(echo $TRAVIS_REPO_SLUG | cut -d "/" -f2 )
 CURRENT_COMMIT=`git rev-parse HEAD`
+
+echo "Targeting $TARGET repository"
 TARGET_URL='https://github.com/thomasdeitz/'$TARGET'.git'
 TARGET_URL_WITH_CREDENTIALS=${TARGET_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github.com}
 
-echo $CURRENT_REPO_SLUG
+echo "Cloning repository from ${TARGET_URL}"
+cd ../
+git clone $TARGET_URL || exit 1
+cd $TARGET || exit 1
+
+echo "Checking out master from ${TARGET}"
+git checkout master || exit 1
+cd ../
+
 echo "Compiling new static content for ${TARGET_URL}"
-
-cd ../
-git clone $TARGET_URL
+harp compile $CURRENT_REPO_SLUG $TARGET || exit 1
 cd $TARGET
-git checkout master
-cd ../
 
-harp compile harp.sl.marketing $TARGET
-cd $TARGET
-git checkout CNAME
-git add -A
-git commit --allow-empty -m "Compiled content for $CURRENT_COMMIT" || exit 1 
-git push --force --quiet "$TARGET_URL_WITH_CREDENTIALS"
+echo "Preserving CNAME"
+git checkout CNAME || exit 1
+
+echo "Commit updates"
+git add -A || exit 1
+git commit --allow-empty -m "Compiled content for $CURRENT_COMMIT" || exit 1
+
+echo "Pushing updates"
+git push --force --quiet "$TARGET_URL_WITH_CREDENTIALS" || exit 1
 
 cd ../
 
 echo "Cleaning up temp files"  
 rm -Rf $TARGET
-rm -Rf harp.sl.marketing
+rm -Rf $CURRENT_REPO_SLUG
 
 echo "Deployed successfully."  
 exit 0 
