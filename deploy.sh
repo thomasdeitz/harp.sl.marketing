a51e846d2f42db95eb2beaee8903c04073005d91
Tom Deitz <info@thomasdeitz.com>
2018-01-23 22:45:32 -0700

    further travis progress github token and more deploys

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 echo "Starting deployment"  
 echo "Target: staging.sl.marketing(staging.socialeads.com) repo"
+
+TEMP_DIRECTORY="/tmp/__temp_static_content"  
+CURRENT_COMMIT=`git rev-parse HEAD`  
+ORIGIN_URL=`git config --get remote.origin.url`  
+ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github.com}
+
+echo CURRENT_COMMIT
