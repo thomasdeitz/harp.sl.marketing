a5df11b03d163cfcf873c7960431effdef974e36
Tom Deitz <info@thomasdeitz.com>
2018-01-24 00:10:11 -0700

    r

--- deploy.sh
+++ deploy.sh
@@ -6,18 +6,30 @@ TEMP_DIRECTORY="/tmp/__temp_static_content"
 CURRENT_COMMIT=`git rev-parse HEAD`  
 ORIGIN_URL=`git config --get remote.origin.url`  
 ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github.com}
-TARGET_URL="https://github.com/thomasdeitz/staging.sl.marketing.git"
+TARGET_URL=$T_URL
 TARGET_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github.com}
+
 echo $T_URL
 echo $ORIGIN_URL
 echo "Compiling new static content"  
 mkdir $TEMP_DIRECTORY || exit 1
-harp compile . $TEMP_DIRECTORY || exit 1
 
-ls -l
 cd ../
-ls -l
-
 git clone $TARGET_URL
 cd staging.sl.marketing
-ls -l
+git checkout master
+cd ../
+
+harp compile harp.sl.marketing staging.sl.marketing
+cd staging.sl.marketing
+git checkout CNAME
+git add -A
+git commit --allow-empty -m "Regenerated static content for $CURRENT_COMMIT" || exit 1 
+git push --force --quiet "$TARGET_URL_WITH_CREDENTIALS"
+
+echo "Cleaning up temp files"  
+rm -Rf staging.sl.marketing
+rm -Rf harp.sl.marketing
+
+echo "Deployed successfully."  
+exit 0 
