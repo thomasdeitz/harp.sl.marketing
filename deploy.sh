8712035c7bb0d0f43b898442f2f9b86848f98f96
Tom Deitz <info@thomasdeitz.com>
2018-01-24 01:44:40 -0700

    repo slug test

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-https://github.com/thomasdeitz/staging.sl.marketing.git
+
 echo "Starting deployment" 
 echo "Getting deployment target."
 
@@ -7,11 +7,13 @@ TARGET=$STAGING_TARGET
 echo $TRAVIS_REPO_SLUG
 echo "Targeting $TARGET repository"
 
+CURRENT_REPO_SLUG=(${TRAVIS_REPO_SLUG//\/\/ })
 CURRENT_COMMIT=`git rev-parse HEAD`
 TARGET_URL='https://github.com/thomasdeitz/'$TARGET'.git'
 TARGET_URL_WITH_CREDENTIALS=${TARGET_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github.com}
 
-echo "Compiling new static content"
+echo $CURRENT_REPO_SLUG
+echo "Compiling new static content for ${TARGET_URL}"
 
 cd ../
 git clone $TARGET_URL
@@ -19,15 +21,17 @@ cd $TARGET
 git checkout master
 cd ../
 
-harp compile harp.sl.marketing staging.sl.marketing
-cd staging.sl.marketing
+harp compile harp.sl.marketing $TARGET
+cd $TARGET
 git checkout CNAME
 git add -A
-git commit --allow-empty -m "Regenerated static content for $CURRENT_COMMIT" || exit 1 
+git commit --allow-empty -m "Compiled content for $CURRENT_COMMIT" || exit 1 
 git push --force --quiet "$TARGET_URL_WITH_CREDENTIALS"
 
+cd ../
+
 echo "Cleaning up temp files"  
-rm -Rf staging.sl.marketing
+rm -Rf $TARGET
 rm -Rf harp.sl.marketing
 
 echo "Deployed successfully."  
