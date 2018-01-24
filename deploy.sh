39e523a94d0e8c2f75b19f90115d48cbfeb74a2e
Tom Deitz <info@thomasdeitz.com>
2018-01-23 23:01:01 -0700

    added target url

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,8 @@ TEMP_DIRECTORY="/tmp/__temp_static_content"
 CURRENT_COMMIT=`git rev-parse HEAD`  
 ORIGIN_URL=`git config --get remote.origin.url`  
 ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github.com}
-
-echo $TEMP_DIRECTORY
-echo $CURRENT_COMMIT
-echo $ORIGIN_URL
-echo $ORIGIN_URL_WITH_CREDENTIALS
+TARGET_URL "staging.sl.marketing.git"
+TARGET_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github.com}
 
 echo "Compiling new static content"  
 mkdir $TEMP_DIRECTORY || exit 1
